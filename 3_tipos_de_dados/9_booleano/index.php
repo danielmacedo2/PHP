9eb1cd8bb99fd86ee5a079081b1288186a944052
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-18 11:43:38 -0300

    Booleanos

---
+++ 3_tipos_de_dados/9_booleano/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo true; // 1 = True 0 = False
+    echo "<br>";
+    echo false;
+    
+    if(true) {
+        echo "É verdadeiro 1! <br>";
+    }
+
+    if(5 > 2) { // true
+        echo "É verdadeiro 2! <br>";
+    }
+
+    $ableToJoin = true;
+
+    if($ableToJoin) {
+        echo "O usuário pode entrar! <br>";
+    }
