dfeaa9254075c41725bd20c7c9da0df46a8191e6
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-19 08:06:38 -0300

    Null

---
+++ 3_tipos_de_dados/17_null/index.php
@@ -0,0 +1,14 @@
+<?php
+    echo NULL;
+
+    $nome = NULL;
+
+    if(is_null($nome)) {
+        echo 'O valor é nulo 1!';
+    }
+
+    $nome = 'Daniel';
+
+    if(is_null($nome)) {
+        echo 'O valor é nulo 2!';
+    }
