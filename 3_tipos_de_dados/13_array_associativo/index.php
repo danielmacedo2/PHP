e6b4f0fc855610861f30f3a887300a3c2884b95f
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-19 07:54:04 -0300

    Arrays

---
+++ 3_tipos_de_dados/13_array_associativo/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $arr = ['nome' => 'Sharky', 'patas' => 4, 'cor' => 'Marrom'];
+
+    echo $arr['nome'];
+    echo '<br>';
+    print_r($arr);
+    echo '<br>';
+    print_r($arr['patas']);
+    echo '<br>';
+
+    $arrAssoc = ['chave' => 'valor', 'bool' => true];
+    print_r($arrAssoc);
